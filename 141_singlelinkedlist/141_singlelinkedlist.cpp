41c81a7ddd030bdf7c36597dccce1fa1046954fc
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-05-18 21:34:19 +0700

    Tambah nim, nama, dan next

--- 141_singlelinkedlist/141_singlelinkedlist.cpp
+++ 141_singlelinkedlist/141_singlelinkedlist.cpp
@@ -104,7 +104,7 @@ void traverse() {
 		Node* currentNode = START;
 		while (currentNode != NULL) {
 			cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
-
+			currentNode = currentNode->next;
 		}
 	}
 }
